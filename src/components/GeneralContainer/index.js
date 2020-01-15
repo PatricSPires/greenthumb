e38ca870567292530253193406d673d4dfcc9695
PatricSPires <patric.pires@outlook.com>
2020-01-15 01:03:38 -0300

    feat(General Container): create a general container omponent

---
+++ src/components/GeneralContainer/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const GeneralContainer = styled.div`
+  @media (min-width: 768px) {
+    width: 100%;
+    max-width: 732px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 80vh;
+  }
+`;
