71b2e0673b77e99d3b757376e4f90574f7fabc25
PatricSPires <patric.pires@outlook.com>
2020-01-15 17:38:35 -0300

    refactor(Main): remove unused code and add route to start quizz

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import { IoIosArrowRoundForward } from 'react-icons/io';
-import { Title, Container, ContainerImage } from './MainStyles';
+import { Title, Container } from './MainStyles';
 import { Button } from '../../components/Button/Button.styled';
 
 import Logo from '../../assets/logo/logo-greenthumb.svg';
-import HeroMobile from '../../assets/illustrations/illustration-home-mobile.png';
 
 export default function Main() {
   return (
@@ -13,14 +12,13 @@ export default function Main() {
       <Container>
         <img className="logo" src={Logo} alt="sadasd" />
         <Title>Find your next green friend</Title>
-        <Button primary>
-          <IoIosArrowRoundForward />
-          start quizz
-        </Button>
+        <a href="/sunlight">
+          <Button primary>
+            <IoIosArrowRoundForward />
+            start quizz
+          </Button>
+        </a>
       </Container>
-      <ContainerImage>
-        <img className="heroMobile" src={HeroMobile} alt="asdasdasd" />
-      </ContainerImage>
     </>
   );
 }
