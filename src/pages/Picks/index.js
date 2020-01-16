3517914f22215e1ae2f276472694114d3604d128
PatricSPires <patric.pires@outlook.com>
2020-01-16 16:56:22 -0300

    feat(picks page): create page

--- src/pages/Picks/index.js
+++ src/pages/Picks/index.js
@@ -1,7 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import { Title, LogoContainer, SunContainer } from './styles';
+import Logo from '../../assets/logo/logo-greenthumb.svg';
+import HandPick from '../../assets/illustrations/pick.png';
+import WaterMark from '../../components/WaterMark';
+import { Button } from '../../components/Button/Button.styled';
 
-export default function Picks() {
-  return <h1>Picks</h1>;
+// Icons
+import toxic from '../../assets/icons/grey/toxic.svg';
+import highSun from '../../assets/icons/grey/highSun.svg';
+import lowSun from '../../assets/icons/grey/lowSun.svg';
+import noSun from '../../assets/icons/grey/noSun.svg';
+import oneDrop from '../../assets/icons/grey/oneDrop.svg';
+import twoDrops from '../../assets/icons/grey/twoDrops.svg';
+import threeDrops from '../../assets/icons/grey/threeDrops.svg';
+
+import {
+  Container,
+  PlantContainer,
+  PlantCard,
+  PlantTitle,
+  PlantPrice,
+  BuyPlant,
+  PlantInformations,
+  PlantAdditionals,
+} from '../../components/PlantCard/style';
+
+import api from '../../services/api';
+
+class Picks extends Component {
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleData();
+  }
+
+  handleData = async e => {
+    const sunlight = JSON.parse(localStorage.getItem('sunlight'));
+    const water = JSON.parse(localStorage.getItem('water'));
+    const pets = JSON.parse(localStorage.getItem('pets'));
+    const response = await api.get(
+      `?sun=${sunlight}&water=${water}&pets=${pets}`
+    );
+
+    this.setState({
+      plants: [...this.state.plants, ...response.data],
+    });
+    console.log(this.state.plants);
+  };
+
+  render() {
+    return (
+      <>
+        <WaterMark />
+        <Container>
+          <LogoContainer>
+            <img src={Logo} alt="greenthumb logo" />
+          </LogoContainer>
+          <SunContainer>
+            <img src={HandPick} alt="Handle picking a plant" />
+          </SunContainer>
+          <Title>Our picks for you</Title>
+          <PlantContainer>
+            {this.state.plants.map(plant => (
+              <PlantCard key={plant.id}>
+                <img src={plant.url} alt={`Plant ${plant.name}`} />
+                <BuyPlant>
+                  <PlantTitle>{plant.name}</PlantTitle>
+                  <PlantInformations>
+                    <PlantPrice>${plant.price}</PlantPrice>
+                    <PlantAdditionals>
+                      {plant.toxicity ? (
+                        <img src={toxic} alt="This plant is toxic" />
+                      ) : (
+                        ''
+                      )}
+                      {plant.sun === 'high' ? <img src={highSun} alt="" /> : ''}
+                      {plant.sun === 'low' ? <img src={lowSun} alt="" /> : ''}
+                      {plant.sun === 'no' ? <img src={noSun} alt="" /> : ''}
+
+                      {plant.water === 'rarely' ? (
+                        <img src={oneDrop} alt="" />
+                      ) : (
+                        ''
+                      )}
+                      {plant.water === 'regularly' ? (
+                        <img src={twoDrops} alt="" />
+                      ) : (
+                        ''
+                      )}
+                      {plant.water === 'daily' ? (
+                        <img src={threeDrops} alt="" />
+                      ) : (
+                        ''
+                      )}
+                    </PlantAdditionals>
+                  </PlantInformations>
+                  <Button>buy now</Button>
+                </BuyPlant>
+              </PlantCard>
+            ))}
+          </PlantContainer>
+        </Container>
+      </>
+    );
+  }
 }
+export default Picks;
---
+++ src/pages/Picks/styles.js
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 40px;
+  max-width: 243px;
+  width: 100%;
+  text-align: center;
+  margin: 0 auto 28px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  color: #0c261c;
+
+  strong {
+    font-weight: bold;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 732px;
+    margin-bottom: 20px;
+    font-size: 30px;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 65px;
+    max-width: 598px;
+    margin-bottom: 78px;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  color: #6e6e6e;
+  font-size: 14px;
+  font-weight: 300;
+  width: 100%;
+  max-width: 254px;
+  margin: 0 auto;
+  text-align: center;
+  margin-bottom: 40px;
+
+  @media (min-width: 768px) {
+    max-width: 640px;
+    margin-bottom: 48px;
+  }
+`;
+
+export const SunContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+  img {
+    width: 146px;
+  }
+
+  @media (min-width: 1024px) {
+    img {
+      width: 226px;
+    }
+  }
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 24px 0;
+
+  img {
+    width: 196px;
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const CardGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  height: 160px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    width: 100%;
+    margin: 0 auto;
+    height: auto;
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 38px;
+
+  @media (min-width: 768px) {
+    width: 80%;
+    margin: 50px auto 0;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
