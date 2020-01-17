f1deda89b9a86837cfe59e40521c11c931991f39
PatricSPires <patric.pires@outlook.com>
2020-01-17 17:56:37 -0300

    feat(purchase): add component styles

---
+++ src/components/Purchase/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Purchase = styled.div`
+  max-width: 381px;
+  width: 100%;
+
+  h1 {
+    font-size: 50px;
+    font-weight: bold;
+    max-width: 377px;
+    text-align: left;
+    margin-bottom: 10px;
+    color: #0c261c;
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: 300;
+    font-style: normal;
+    color: #6e6e6e;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
