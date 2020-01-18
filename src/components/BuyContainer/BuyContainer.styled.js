add7f50f0faa058cd7f2dbc77d9e115ca0a83ca8
PatricSPires <patric.pires@outlook.com>
2020-01-18 14:26:15 -0300

    feat(BuyPlant): create a component

---
+++ src/components/BuyContainer/BuyContainer.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const BuyContainer = styled.div`
+  max-width: 864px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 138px;
+`;
--- src/pages/Plant/index.js
+++ src/pages/Plant/index.js
@@ -3,7 +3,6 @@ import api from '../../services/api';
 
 import { Purchase } from '../../components/Purchase';
 import Price from '../../components/Price';
-import { Container } from './styles';
 
 import toxic from '../../assets/icons/grey/toxic.svg';
 import highSun from '../../assets/icons/grey/highSun.svg';
@@ -15,7 +14,8 @@ import threeDrops from '../../assets/icons/grey/threeDrops.svg';
 
 import Form from '../../components/Form';
 
-// import WaterMark from '../../components/WaterMark';
+import Container from '../../components/Container';
+import { BuyContainer } from '../../components/BuyContainer/BuyContainer.styled';
 
 export default class Plant extends Component {
   constructor() {
@@ -32,49 +32,49 @@ export default class Plant extends Component {
     this.setState({
       plant: response.data,
     });
-    console.log(response);
   }
 
   render() {
     return (
       <Container>
-        {/* <WaterMark /> */}
-        <Purchase>
-          <h1>{this.state.plant.name}</h1>
-          <Price price={this.state.plant.price} />
-          <img
-            src={this.state.plant.url}
-            alt={`This plant is: ${this.state.plant.name}`}
-          />
-          <div>
-            {this.state.plant.sun === 'high' && <img src={highSun} alt="" />}
-            {this.state.plant.sun === 'low' && <img src={lowSun} alt="" />}
-            {this.state.plant.sun === 'no' && <img src={noSun} alt="" />}
+        <BuyContainer>
+          <Purchase>
+            <h1>{this.state.plant.name}</h1>
+            <Price price={this.state.plant.price} />
+            <img
+              src={this.state.plant.url}
+              alt={`This plant is: ${this.state.plant.name}`}
+            />
+            <div>
+              {this.state.plant.sun === 'high' && <img src={highSun} alt="" />}
+              {this.state.plant.sun === 'low' && <img src={lowSun} alt="" />}
+              {this.state.plant.sun === 'no' && <img src={noSun} alt="" />}
 
-            {this.state.plant.water === 'rarely' ? (
-              <img src={oneDrop} alt="" />
-            ) : (
-              ''
-            )}
-            {this.state.plant.water === 'regularly' ? (
-              <img src={twoDrops} alt="" />
-            ) : (
-              ''
-            )}
-            {this.state.plant.water === 'daily' ? (
-              <img src={threeDrops} alt="" />
-            ) : (
-              ''
-            )}
+              {this.state.plant.water === 'rarely' ? (
+                <img src={oneDrop} alt="" />
+              ) : (
+                ''
+              )}
+              {this.state.plant.water === 'regularly' ? (
+                <img src={twoDrops} alt="" />
+              ) : (
+                ''
+              )}
+              {this.state.plant.water === 'daily' ? (
+                <img src={threeDrops} alt="" />
+              ) : (
+                ''
+              )}
 
-            {this.state.plant.toxicity ? (
-              <img src={toxic} alt="This plant is toxic" />
-            ) : (
-              ''
-            )}
-          </div>
-        </Purchase>
-        <Form />
+              {this.state.plant.toxicity ? (
+                <img src={toxic} alt="This plant is toxic" />
+              ) : (
+                ''
+              )}
+            </div>
+          </Purchase>
+          <Form />
+        </BuyContainer>
       </Container>
     );
   }
