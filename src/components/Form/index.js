2ac397fe068d9e92f4fd2fb3e2037076f9375a54
PatricSPires <patric.pires@outlook.com>
2020-01-19 13:54:25 -0300

    feat(Form): create sent mail screen

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
-import { FormComponent } from './Form.styled';
+import { FormContainer, FormComponent, EmailSent } from './Form.styled';
 import { Button } from '../Button/Button.styled';
 import Label from '../Label';
 import Input from '../Input';
+import Envelop from '../../assets/illustrations/envelop.png';
 
 export default class Form extends Component {
   constructor() {
@@ -16,18 +17,25 @@ export default class Form extends Component {
 
   render() {
     return (
-      <FormComponent>
-        <h1>Nice pick!</h1>
-        <p>
-          Tell us your name and e-mail and we will get in touch about your order
-          ;)
-        </p>
-        <Label text="Name" labelFor="name" />
-        <Input placeholder="Crazy Plant Person" type="text" />
-        <Label text="E-mail" />
-        <Input placeholder="plantperson@email.com" type="email" />
-        <Button type="button">send</Button>
-      </FormComponent>
+      <FormContainer>
+        <FormComponent>
+          <h1>Nice pick!</h1>
+          <p>
+            Tell us your name and e-mail and we will get in touch about your
+            order ;)
+          </p>
+          <Label text="Name" labelFor="name" />
+          <Input placeholder="Crazy Plant Person" type="text" />
+          <Label text="E-mail" />
+          <Input placeholder="plantperson@email.com" type="email" />
+          <Button type="button">send</Button>
+        </FormComponent>
+        <EmailSent>
+          <h1>Thank you!</h1>
+          <p>You will hear from us soon. Please check your e-mail!</p>
+          <img src={Envelop} alt="" />
+        </EmailSent>
+      </FormContainer>
     );
   }
 }
