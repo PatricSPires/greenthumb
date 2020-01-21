b7c397e7fc6d845d989f9e7a5c68f78efc18b9c0
PatricSPires <patric.pires@outlook.com>
2020-01-21 20:05:04 -0300

    refactor(Input): use variables file

--- src/components/Input/Input.styled.js
+++ src/components/Input/Input.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import { colors, fontSize } from '../../shared/variables';
 
 export const InputComponent = styled.input`
-  background-color: #ffffff;
+  background-color: ${colors.white};
   max-width: 297px;
   border-radius: 19px;
-  font-size: 14px;
-  color: #d0d0d0;
+  font-size: ${fontSize['u-fontSize-14']};
+  color: ${colors.alto};
   border: none;
   padding: 6px 75px 6px 25px;
 `;
