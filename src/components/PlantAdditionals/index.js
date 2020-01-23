2c8449a0b0147f9ae701c5de7027de3159332b69
PatricSPires <patric.pires@outlook.com>
2020-01-23 13:26:46 -0300

    refactor(PlantAdditionals): update icons

--- src/components/PlantAdditionals/index.js
+++ src/components/PlantAdditionals/index.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 import { PlantAdditionalsComponent } from './PlantAdditionals.styled';
 
-import toxic from '../../assets/icons/grey/toxic.svg';
-import highSun from '../../assets/icons/grey/highSun.svg';
-import lowSun from '../../assets/icons/grey/lowSun.svg';
-import noSun from '../../assets/icons/grey/noSun.svg';
-import oneDrop from '../../assets/icons/grey/oneDrop.svg';
-import twoDrops from '../../assets/icons/grey/twoDrops.svg';
-import threeDrops from '../../assets/icons/grey/threeDrops.svg';
+import { ReactComponent as Toxic } from '../../assets/icons/grey/toxic.svg';
+import { ReactComponent as HighSun } from '../../assets/icons/grey/highSun.svg';
+import { ReactComponent as LowSun } from '../../assets/icons/grey/lowSun.svg';
+import { ReactComponent as NoSun } from '../../assets/icons/grey/noSun.svg';
+import { ReactComponent as OneDrop } from '../../assets/icons/grey/oneDrop.svg';
+import { ReactComponent as TwoDrops } from '../../assets/icons/grey/twoDrops.svg';
+import { ReactComponent as ThreeDrops } from '../../assets/icons/grey/threeDrops.svg';
 
 export default function PlantAdditionals({ sun, water, toxicity }) {
   return (
     <PlantAdditionalsComponent>
-      {toxicity ? <img src={toxic} alt="This plant is toxic" /> : ''}
-      {sun === 'high' && <img src={highSun} alt="" />}
-      {sun === 'low' && <img src={lowSun} alt="" />}
-      {sun === 'no' && <img src={noSun} alt="" />}
+      {toxicity && <Toxic />}
+      {sun === 'high' && <HighSun />}
+      {sun === 'low' && <LowSun />}
+      {sun === 'no' && <NoSun />}
 
-      {water === 'rarely' ? <img src={oneDrop} alt="" /> : ''}
-      {water === 'regularly' ? <img src={twoDrops} alt="" /> : ''}
-      {water === 'daily' ? <img src={threeDrops} alt="" /> : ''}
+      {water === 'rarely' && <OneDrop />}
+      {water === 'regularly' && <TwoDrops />}
+      {water === 'daily' && <ThreeDrops />}
     </PlantAdditionalsComponent>
   );
 }
