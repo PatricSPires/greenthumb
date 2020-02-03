806817cf590f27395fecfc67221c91fd37d6eda7
PatricSPires <patric.pires@outlook.com>
2020-02-03 01:33:54 -0300

    refactor(plant): use react hooks

--- src/pages/Plant/index.js
+++ src/pages/Plant/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 
 import { Purchase } from '../../components/Purchase';
@@ -22,102 +22,98 @@ import { Informations } from './PlantStyles';
 import Logo from '../../components/Logo';
 import Header from '../../components/Header';
 
-export default class Plant extends Component {
-  constructor() {
-    super();
-    this.state = {
-      plant: [],
-    };
-  }
+function Plant() {
+  const [plant, setPlant] = useState([]);
 
-  async componentDidMount() {
-    const selectedPlant = localStorage.getItem('plant');
+  useEffect(() => {
+    async function getPlantInformation() {
+      const selectedPlant = localStorage.getItem('plant');
+      const response = await api.get(`/plant?id=${selectedPlant}`);
+      setPlant(response.data);
+    }
+    getPlantInformation();
+  }, []);
 
-    const response = await api.get(`/plant?id=${selectedPlant}`);
-    this.setState({
-      plant: response.data,
-    });
-  }
+  const { name, price, url, sun, water, toxicity } = plant;
 
-  render() {
-    const { name, price, url, sun, water, toxicity } = this.state.plant;
-    return (
-      <Container>
-        <Header>
-          <Logo />
-        </Header>
-        <BuyContainer>
-          <Purchase>
-            <h1>{name}</h1>
-            <Price price={price} fontSize={24} />
-            <img src={url} alt={`This plant is: ${name}`} />
-            <Informations>
-              {sun === 'high' ? (
-                <PlantInformation>
-                  <HighSun />
-                  High Sunlight
-                </PlantInformation>
-              ) : (
-                ''
-              )}
-              {sun === 'low' ? (
-                <PlantInformation>
-                  <LowSun />
-                  Low Sunlight
-                </PlantInformation>
-              ) : (
-                ''
-              )}
-              {sun === 'no' ? (
-                <PlantInformation>
-                  <NoSun />
-                  No Sunlight
-                </PlantInformation>
-              ) : (
-                ''
-              )}
+  return (
+    <Container>
+      <Header>
+        <Logo />
+      </Header>
+      <BuyContainer>
+        <Purchase>
+          <h1>{name}</h1>
+          <Price price={price} fontSize={24} />
+          <img src={url} alt={`This plant is: ${name}`} />
+          <Informations>
+            {sun === 'high' ? (
+              <PlantInformation>
+                <HighSun />
+                High Sunlight
+              </PlantInformation>
+            ) : (
+              ''
+            )}
+            {sun === 'low' ? (
+              <PlantInformation>
+                <LowSun />
+                Low Sunlight
+              </PlantInformation>
+            ) : (
+              ''
+            )}
+            {sun === 'no' ? (
+              <PlantInformation>
+                <NoSun />
+                No Sunlight
+              </PlantInformation>
+            ) : (
+              ''
+            )}
 
-              {water === 'rarely' ? (
-                <PlantInformation>
-                  <OneDrop />
-                  Water Rarely
-                </PlantInformation>
-              ) : (
-                ''
-              )}
-              {water === 'regularly' ? (
-                <PlantInformation>
-                  <TwoDrops />
-                  Water Regularly
-                </PlantInformation>
-              ) : (
-                ''
-              )}
-              {water === 'daily' ? (
-                <PlantInformation>
-                  <ThreeDrops />
-                  Water Daily
-                </PlantInformation>
-              ) : (
-                ''
-              )}
+            {water === 'rarely' ? (
+              <PlantInformation>
+                <OneDrop />
+                Water Rarely
+              </PlantInformation>
+            ) : (
+              ''
+            )}
+            {water === 'regularly' ? (
+              <PlantInformation>
+                <TwoDrops />
+                Water Regularly
+              </PlantInformation>
+            ) : (
+              ''
+            )}
+            {water === 'daily' ? (
+              <PlantInformation>
+                <ThreeDrops />
+                Water Daily
+              </PlantInformation>
+            ) : (
+              ''
+            )}
 
-              {toxicity ? (
-                <PlantInformation>
-                  <Toxic />
-                  <strong>Beware!</strong> &nbsp;Toxic for pets
-                </PlantInformation>
-              ) : (
-                <PlantInformation>
-                  <Pet />
-                  Non-toxic for pets
-                </PlantInformation>
-              )}
-            </Informations>
-          </Purchase>
-          <Form />
-        </BuyContainer>
-      </Container>
-    );
-  }
+            {toxicity ? (
+              <PlantInformation>
+                <Toxic />
+                <strong>Beware!</strong> &nbsp;Toxic for pets
+              </PlantInformation>
+            ) : (
+              <PlantInformation>
+                <Pet />
+                Non-toxic for pets
+              </PlantInformation>
+            )}
+          </Informations>
+        </Purchase>
+        <Form />
+      </BuyContainer>
+    </Container>
+  );
 }
+
+export default Plant;
