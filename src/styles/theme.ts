0b91d147d21bd868783aeab975fd77dbd56cc9e0
PatricSPires <patric.pires@outlook.com>
2021-02-11 23:52:45 -0300

    feat: create initial theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,36 @@
+export default {
+  colors: {
+    green_pea: '#15573f',
+    bush: '#0c261c',
+    wild_sand: '#f6f6f6',
+    alto: '#d0d0d0',
+    alto_six: '#d6d6d6',
+    dove_gray: '#6e6e6e',
+    bg_light: 'rgba(0, 0, 0, 0.0758577)',
+    bg_shadow: 'rgba(0, 0, 0, 0.0896799)',
+    bg_dark: 'rgba(0, 0, 0, 0.25)',
+    white: '#ffffff',
+    silver_chalice: '#acacac',
+    atomic_tangerine: '#fd9872',
+    acapulco: '#7BAF9E',
+    concrete: '#f2f2f2',
+    torch_red: '#f71d1d'
+  },
+  fontSize: {
+    8: '0.5em',
+    12: '0.75em',
+    14: '0.875em',
+    16: '1em',
+    18: '1em',
+    22: '1.375em',
+    24: '1.5em',
+    30: '1.875em',
+    32: '2em',
+    35: '2.1875em',
+    36: '2.25em',
+    40: '2.5em',
+    50: '3.125em',
+    65: '4.0625em',
+    66: '4.125em'
+  }
+}
